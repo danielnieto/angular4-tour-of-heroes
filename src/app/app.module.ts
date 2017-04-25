fc9d28fd46168f8a0eac736ed051949b2374f573
Daniel Nieto <danielnieto89@gmail.com>
2017-04-25 16:56:58 -0500

    use Router to navigate between Dashboard and Heroes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { RouterModule }   from '@angular/router';
+import { HeroService } from "./hero.service";
 
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent],
+  imports:      [ BrowserModule,
+                    FormsModule,
+                    RouterModule.forRoot([
+                        {
+                            path: '',
+                            redirectTo: '/dashboard',
+                            pathMatch: 'full'
+                        },
+                        {
+                            path: 'heroes',
+                            component: HeroesComponent
+                        },
+                        {
+                            path: 'dashboard',
+                            component: DashboardComponent
+                        }
+                  ]),
+                 ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
