5ca2f9d9413e2235aa8ccfa84269f12d4ab85bfb
Daniel Nieto <danielnieto89@gmail.com>
2017-04-25 13:55:03 -0500

    refactor Detail’s view into a separate HeroDetailComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
@@ -73,14 +74,7 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
     </ul>
-    <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-        <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-    </div>
+     <hero-detail [hero]="selectedHero"></hero-detail>
   `,
 })
 export class AppComponent {
