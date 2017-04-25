d842421a80bd76930e8c89da5eee8f78aa527689
Daniel Nieto <danielnieto89@gmail.com>
2017-04-25 14:59:31 -0500

    use Promises for HeroService’s methods

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,7 +6,14 @@ import {HEROES} from "./mock-heroes";
 
 @Injectable()
 export class HeroService{
-    getHeroes():Hero[]{
-        return HEROES;
+    getHeroes():Promise<Hero[]>{
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            // simulate latency
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
     }
 }
