ab7b0c87c015c4bbc4bfa1946bc205c5838b7e40
Daniel Nieto <danielnieto89@gmail.com>
2017-04-28 13:20:02 -0500

    use HTTP Services and In Memory Web API to serve heroes collection
    simulate web API communication instead of using a mock file for heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
@@ -11,7 +17,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   providers: [HeroService],
   bootstrap:    [ AppComponent ]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,27 @@
 import {Injectable} from "@angular/core";
-
+import { Headers, Http } from '@angular/http';
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
+
+import 'rxjs/add/operator/toPromise';
 
 
 @Injectable()
 export class HeroService{
+
+    private heroesUrl = 'api/heroes';  // URL to web api
+
+    constructor(private http: Http){}
+
     getHeroes():Promise<Hero[]>{
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+                .toPromise()
+                .then(response => response.json().data as Hero[])
+                .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any>{
+        console.error("An error ocurred", error);
+        return Promise.reject(error.message || error);
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
